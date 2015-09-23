9d6490a5ff66e636ca4d7eaef7682da98edf1b6e
Kirill Morozov <tiguhrrr@gmail.com>
2015-09-23 17:58:22 +0300

    Add Unlink Provider Feature.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express    = require('express');
 var signup     = require('./auth/signup');
 var login      = require('./auth/login');
 var google     = require('./auth/google');
+var unlink     = require('./auth/unlink');
 var twitter    = require('./auth/twitter');
 var facebook   = require('./auth/facebook');
 var instagram  = require('./auth/instagram');
@@ -11,6 +12,7 @@ router.use(instagram);
 router.use(facebook);
 router.use(twitter);
 router.use(google);
+router.use(unlink);
 router.use(signup);
 router.use(login);
 
---
+++ routes/auth/unlink.js
@@ -0,0 +1,25 @@
+var User    = require('../../models/user');
+var express = require('express');
+var limit   = require('../middleware/limit');
+var router  = express.Router();
+
+router.post('/unlink', limit, function (req, res) {
+  var provider  = req.body.provider;
+  var providers = ['facebook', 'google', 'instagram', 'twitter'];
+
+  if (providers.indexOf(provider) === -1) {
+    return res.status(400).send({ message : 'Unknown OAuth Provider' });
+  }
+
+  User.findById(req.user, function (req, res) {
+    if (!user) {
+      return res.status(400).send({ message : 'User Not Found' });
+    }
+    user[provider] = undefined;
+    user.save(function () {
+      res.status(200).end();
+    });
+  });
+});
+
+module.exports = router;
